63389aea34c1b5f5f154910728574c5860c51276
Felix Zumstein <fzumstein@gmail.com>
2023-05-16 11:31:31 +0200

    use Book as context manager

--- app/api/github.py
+++ app/api/github.py
@@ -35,60 +35,60 @@ async def analyze_issues(
     logger.info(f"Running query for {current_user.email}")
 
     # Google Sheet objects
-    book = xw.Book(json=data)
-    dashboard_sheet = book.sheets["Dashboard"]
-    issues_sheet = book.sheets["Open Issues"]
-    charts_sheet = book.sheets["Charts"]
-    repo_name = dashboard_sheet["B12"].value
-    if not repo_name or "/" not in repo_name:
-        raise HTTPException(
-            status.HTTP_400_BAD_REQUEST,
-            detail="Please provide a repo in the form 'owner/repo'.",
+    with xw.Book(json=data) as book:
+        dashboard_sheet = book.sheets["Dashboard"]
+        issues_sheet = book.sheets["Open Issues"]
+        charts_sheet = book.sheets["Charts"]
+        repo_name = dashboard_sheet["B12"].value
+        if not repo_name or "/" not in repo_name:
+            raise HTTPException(
+                status.HTTP_400_BAD_REQUEST,
+                detail="Please provide a repo in the form 'owner/repo'.",
+            )
+
+        # Total and closed issues over time in monthly buckets
+        issues = await get_issues(repo_name)
+        issues_per_month = []
+        for col_name in ["created_at", "closed_at"]:
+            issues[col_name] = pd.to_datetime(issues[col_name])
+            issues_per_month.append(issues.set_index(col_name)["id"].resample("M").count())
+        issues_per_month = pd.concat(issues_per_month, axis=1)
+        issues_per_month.columns = ["Total Issues", "Closed Issues"]
+        cumulative_issues_per_month = issues_per_month.cumsum()
+        cumulative_issues_per_month = cumulative_issues_per_month.fillna(method="ffill")
+        issues_state_value_counts = issues["state"].value_counts()
+
+        # Write results back to Google Sheets
+        dashboard_sheet["G3"].value = issues_state_value_counts.get("open")
+        dashboard_sheet["G7"].value = issues_state_value_counts.get("closed")
+
+        charts_target_cell = charts_sheet["A1"]
+        charts_target_cell.expand().clear_contents()
+        charts_target_cell.value = cumulative_issues_per_month
+
+        issues_target_cell = issues_sheet["A1"]
+        issues_target_cell.expand().clear_contents()
+        issues.columns = [name.replace("_", " ").capitalize() for name in issues.columns]
+        issues_target_cell.options(index=False).value = issues.loc[
+            issues["State"] == "open", :
+        ].drop(columns=["Id", "State", "Closed at"])
+
+        # Matplotlib plot
+        plt.style.use("fivethirtyeight")
+        ax = cumulative_issues_per_month.plot.area(
+            figsize=(9, 6), stacked=False, color=["#4185f4", "#ea4435"]
+        )
+        fig = ax.get_figure()
+        fig.suptitle("Total vs. Closed Issues", fontsize=14, fontweight="bold")
+        dashboard_sheet.pictures.add(
+            image=fig,
+            name="time_series",
+            anchor=dashboard_sheet["I2"],
+            export_options={"bbox_inches": "tight", "dpi": 80},
+            update=True,
         )
 
-    # Total and closed issues over time in monthly buckets
-    issues = await get_issues(repo_name)
-    issues_per_month = []
-    for col_name in ["created_at", "closed_at"]:
-        issues[col_name] = pd.to_datetime(issues[col_name])
-        issues_per_month.append(issues.set_index(col_name)["id"].resample("M").count())
-    issues_per_month = pd.concat(issues_per_month, axis=1)
-    issues_per_month.columns = ["Total Issues", "Closed Issues"]
-    cumulative_issues_per_month = issues_per_month.cumsum()
-    cumulative_issues_per_month = cumulative_issues_per_month.fillna(method="ffill")
-    issues_state_value_counts = issues["state"].value_counts()
-
-    # Write results back to Google Sheets
-    dashboard_sheet["G3"].value = issues_state_value_counts.get("open")
-    dashboard_sheet["G7"].value = issues_state_value_counts.get("closed")
-
-    charts_target_cell = charts_sheet["A1"]
-    charts_target_cell.expand().clear_contents()
-    charts_target_cell.value = cumulative_issues_per_month
-
-    issues_target_cell = issues_sheet["A1"]
-    issues_target_cell.expand().clear_contents()
-    issues.columns = [name.replace("_", " ").capitalize() for name in issues.columns]
-    issues_target_cell.options(index=False).value = issues.loc[
-        issues["State"] == "open", :
-    ].drop(columns=["Id", "State", "Closed at"])
-
-    # Matplotlib plot
-    plt.style.use("fivethirtyeight")
-    ax = cumulative_issues_per_month.plot.area(
-        figsize=(9, 6), stacked=False, color=["#4185f4", "#ea4435"]
-    )
-    fig = ax.get_figure()
-    fig.suptitle("Total vs. Closed Issues", fontsize=14, fontweight="bold")
-    dashboard_sheet.pictures.add(
-        image=fig,
-        name="time_series",
-        anchor=dashboard_sheet["I2"],
-        export_options={"bbox_inches": "tight", "dpi": 80},
-        update=True,
-    )
-
-    return book.json()
+        return book.json()
 
 
 async def fetch(url: str, client: httpx.AsyncClient) -> httpx.Response:
